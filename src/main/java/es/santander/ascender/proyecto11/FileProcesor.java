41670ae699587fd357be256a30b438b10ef58815
curso04 <curso04@gmail.com>
2024-11-29 13:38:43 +0100

    implementación métodos

---
+++ src/main/java/es/santander/ascender/proyecto11/FileProcesor.java
@@ -0,0 +1,23 @@
+package es.santander.ascender.proyecto11;
+
+public class FileProcesor implements IFileProcesor{
+
+    @Override
+    public String leerFile(String filePath) throws Exception {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'leerFile'");
+    }
+
+    @Override
+    public String eliminarVocales(String input) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'eliminarVocales'");
+    }
+
+    @Override
+    public void escribirAFile(String filePath, String content) throws Exception {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'escribirAFile'");
+    }
+
+}
