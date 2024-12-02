e73a592547735dfc15f0998b07fdbe2f4434c5bc
Juanma <Juanma888@gmail.com>
2024-12-02 19:35:08 +0100

    Versión final sin comentarios. ¡Ay!

--- src/main/java/es/santander/ascender/proyecto11/FileProcesor.java
+++ src/main/java/es/santander/ascender/proyecto11/FileProcesor.java
@@ -10,15 +10,9 @@ public class FileProcesor implements IFileProcesor{
 
     @Override
     public String leerFile(String filePath) throws Exception {
-        // Tengo que devolver un String
-        
-        // Método readAllLines devuelve un tipo List<String>
-        // Convertirlo en String para retornar el valor de salida del método.
 
-        // Argumento de readAllLines es de tipo Path, => pasar el argumento recibido filePath a tipo Path
         Path camino = Path.of(filePath);
 
-        // Files.readAllLines devuelve un tipo List<String>. Importamos paquete java.util.List;
         List<String> ficheroEnt = Files.readAllLines(camino);
         String salida = String.join("\n", ficheroEnt);
         return salida;
@@ -26,7 +20,7 @@ public class FileProcesor implements IFileProcesor{
 
     @Override
     public String eliminarVocales(String input) {
-        // Recibimos una cadena String y devolvemos otra sin las vocales.
+
         String sinVocales="";
         for (int i=0;i<input.length();i++){
             switch (input.charAt(i)){
@@ -42,31 +36,11 @@ public class FileProcesor implements IFileProcesor{
         }
         return sinVocales;
     }
-/*
-    public static void escribirAFichero(String contenido) throws IOException {
-        Path path = Paths.get("/tmp/testEscribir.txt"); // Asegúrate de que la ruta sea válida
-        
-        // Escribe el contenido en el archivo, si el archivo no existe se crea automáticamente
-        Files.write(path, contenido.getBytes(), StandardOpenOption.CREATE, StandardOpenOption.WRITE);
-    }
-} 
-     */ 
+
     @Override
     public void escribirAFile(String filePath, String content) throws Exception {
-        
-        // Crear archivo temporal de prueba
-        Path ficheroTemp = Path.of(filePath);
 
-        // Escribir contenido en el archivo temporal
-        // procesor.escribirAFile(tempFile.toString(), content);
-        
-        // Clase Files, método write con parámetros (Path, arreglo de bytes del contenido )
-        // Antes transformar contenido pasado por parámetro a formato String, que es el que
-        // requiere 'write'; (un arreglo de bytes). String tiene el método getBytes()
-        // Escribir contenido en el archivo temporal.
-        
-        // OJO: Lo crea en /tmp, pero si no tiene todos los permisos, será inútil.
-        // sudo chmod 1777 /tmp 
+        Path ficheroTemp = Path.of(filePath);
 
         try {
         Files.write(ficheroTemp, content.getBytes(), StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING);
@@ -74,43 +48,10 @@ public class FileProcesor implements IFileProcesor{
         throw new Exception("Error escribiendo el archivo: " + e.getMessage(), e);
           }  
 
-        /* Leer el contenido del archivo temporal para verificar
-          String fileContent = Files.readString(tempFile);
-          Almacenamos el contenido del fichero en un String para verlo en depuración 
-          ANTES de finalmente ser borrado.
-        */
-
         String contenidoFichero = Files.readString(ficheroTemp);
 
-        
-        // Eliminar archivo temporal después de la prueba
-
         // Files.delete(ficheroTemp);
-        // Dejamos que sea el test el que lo elimine, para
-        // no provocar errores como que intente leer el archivo y ya no esté
+        // Dejamos que sea el test el que lo elimine, para no
+        // provocar errores como que intente leer el archivo y ya no esté. Ejem.
     }
-}
-    /* POR MOTIVOS DIDÁCTICOS AÑADO ESTE TEXTO AL FINAL DEL CÓDIGO.
-
-    JAVA 8 API
-    java.nio.file.Files -> clase
-    java.nio.file.Path (en singular) -> interfaz
-
-    
-    public static List<String> readAllLines(Path path)
-                                    throws IOException
-
-    Read all lines from a file. Bytes from the file are decoded into characters using the UTF-8 charset. 
-
-
-
-    public static Path createTempFile(String prefix,
-                                  String suffix,
-                                  FileAttribute<?>... attrs)
-                           throws IOException
-
-    Creates an empty file in the default temporary-file directory, using the given prefix and suffix to 
-    generate its name. The resulting Path is associated with the default FileSystem. 
-
-
-    */
+}    
--- src/main/java/es/santander/ascender/proyecto11/GestionEstudiantes.java
+++ src/main/java/es/santander/ascender/proyecto11/GestionEstudiantes.java
@@ -4,13 +4,11 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
-// Implemento la interfaz. Le digo que agrege los métodos de la misma desde la bombillita
+
 public class GestionEstudiantes implements IGestionEstudiantes{
 
     private Map<String, Integer> estudiantesMap;
     private Set<String> estudiantesSet;  
-    // En los ejemplos vistos hasta ahora, la declaración e inicialización de la instancia
-    // los hacía dentro del main y podía usar sus métodos a continuación. Estos métodos no son estáticos.
     
     public GestionEstudiantes(){
         estudiantesMap = new HashMap<String,Integer>();
@@ -18,10 +16,9 @@ public class GestionEstudiantes implements IGestionEstudiantes{
     }
     @Override
     public boolean agregarEstudiante(String nombre, int calificacion) {
-    // El test pide comprobar (true/false) si ya existe el estudiante para no duplicar la entrada. Si es posible, agregar.
-    
+        
               if (estudiantesMap.containsKey(nombre)){
-                return false; // Ya existe luego no es posible agregarlo.
+                return false; 
               } 
                 estudiantesMap.put(nombre,calificacion);
                 return true;
@@ -30,13 +27,10 @@ public class GestionEstudiantes implements IGestionEstudiantes{
     @Override
     public Integer obtenerCalificacion(String nombre) {
         return estudiantesMap.get(nombre);
-        // devuelve el par-valor del parámetro nombre, que es calificación, un entero.
     }
 
     @Override
     public Map<String, Integer> obtenerEstudiantesYCalificaciones() {
-        // estudiantesMap.put("Pepe del Uno",99);
-        // estudiantesMap.put("Pepe del Dos",68);
         // El test ya aporta las dos entradas.
         return estudiantesMap;
     }
